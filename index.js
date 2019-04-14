aa8ef54d5f026867b7e5ac47c6c1708adae445ef
levy <897895407@qq.com>
2019-04-14 15:13:51 +0800

    issue_list done

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+require('dotenv').config()
+
 const Koa = require('koa');
 const app = new Koa();
 const port = process.env.PORT || 3000
 
+const axios = require('axios')
+const axiosConfig = {
+  method: 'POST',
+  url: 'https://api.github.com/graphql',
+  headers: {
+  	Authorization: 'bearer ' + process.env.GITHUB_TOKEN
+  },
+}
+
 // response
-app.use(ctx => {
+app.use(async ctx => {
   if(ctx.path == '/') {
 	  ctx.body = 'please request valid path';
 		return
 	}
 
   if (ctx.path == '/issue_list') {
-	  ctx.body = 'list'
+    let resp = await axios(Object.assign({}, axiosConfig, {
+		  data: {
+			  "query": "query {\n repository(owner:\"levy9527\", name:\"blog\") {\n issues(first: 50,) {\n edges {\n node {\n title \n url \n number \n}\n}\n}\n}\n rateLimit {\n cost \n remaining \n resetAt \n}\n}"
+			}
+		}))
+
+	  ctx.body = resp.data.data
 		return
 	}
 
